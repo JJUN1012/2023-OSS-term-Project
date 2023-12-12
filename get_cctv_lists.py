ef8d431cf43d9ddcdf59545b4b6aa023f6a3dc8a
JJUN <junnisbada@gachon.ac.kr>
2023-12-12 16:31:34 +0900

    mode changed

--- get_cctv_lists.py
+++ get_cctv_lists.py
@@ -41,7 +41,7 @@ def get_cctv_lists():
     #API call
     api_call = 'https://openapi.its.go.kr:9443/cctvInfo?'\
         'apiKey=' + api_key +\
-        '&type=ex&cctvType=1'\
+        '&type=ex&cctvType=3'\
         '&minX='+ str(minX) +\
         '&maxX='+ str(maxX) +\
         '&minY='+ str(minY) +\
